b373e74e38c80fe07ecb8d6fda0e3934fd6a6f82
Gustavo <gustavo27hs@gmail.com>
2023-07-24 16:05:54 -0300

    js: implementacao de modulos do quiz

---
+++ script/module/livros.js
@@ -0,0 +1,102 @@
+export const perguntasLivros = [
+    {
+        pergunta: "1. Qual livro é conhecido como a obra máxima da literatura brasileira, escrito por Machado de Assis?",
+        respostas: {
+            a: "Dom Casmurro",
+            b: "O Primo Basílio",
+            c: "Grande Sertão: Veredas",
+            d: "Memórias Póstumas de Brás Cubas",
+        },
+        respostaCorreta: "d",
+    },
+    {
+        pergunta: "2. Em qual livro um jovem bruxo é aceito na Escola de Magia e Bruxaria de Hogwarts?",
+        respostas: {
+            a: "Cem Anos de Solidão",
+            b: "O Hobbit",
+            c: "Harry Potter e a Pedra Filosofal",
+            d: "1984",
+        },
+        respostaCorreta: "c",
+    },
+    {
+        pergunta: "3. Qual livro é uma distopia que se passa em um futuro sombrio e totalitário?",
+        respostas: {
+            a: "Harry Potter e as Relíquias da Morte",
+            b: "1984",
+            c: "O Senhor dos Anéis: A Sociedade do Anel",
+            d: "O Pequeno Príncipe",
+        },
+        respostaCorreta: "b",
+    },
+    {
+        pergunta: "4. Qual livro conta a história de um garoto que se torna famoso ao sobreviver a um ataque de um bruxo das trevas?",
+        respostas: {
+            a: "Cem Anos de Solidão",
+            b: "Harry Potter e o Cálice de Fogo",
+            c: "O Hobbit",
+            d: "O Primo Basílio",
+        },
+        respostaCorreta: "b",
+    },
+    {
+        pergunta: "5. Qual livro é uma obra-prima da literatura colombiana, escrita por Gabriel García Márquez?",
+        respostas: {
+            a: "O Primo Basílio",
+            b: "Dom Casmurro",
+            c: "O Pequeno Príncipe",
+            d: "Cem Anos de Solidão",
+        },
+        respostaCorreta: "d",
+    },
+    {
+        pergunta: "6. Em qual livro um menino embarca em uma jornada com um grupo de anões e um mago para recuperar um tesouro?",
+        respostas: {
+            a: "O Senhor dos Anéis: O Retorno do Rei",
+            b: "1984",
+            c: "Harry Potter e as Relíquias da Morte",
+            d: "O Hobbit",
+        },
+        respostaCorreta: "d",
+    },
+    {
+        pergunta: "7. Qual livro retrata a jornada de Frodo Baggins para destruir um poderoso anel maligno?",
+        respostas: {
+            a: "1984",
+            b: "O Pequeno Príncipe",
+            c: "O Senhor dos Anéis: A Sociedade do Anel",
+            d: "O Primo Basílio",
+        },
+        respostaCorreta: "c",
+    },
+    {
+        pergunta: "8. Qual livro é um clássico da literatura portuguesa, escrito por José de Alencar?",
+        respostas: {
+            a: "Dom Casmurro",
+            b: "O Guarani",
+            c: "Memórias Póstumas de Brás Cubas",
+            d: "O Pequeno Príncipe",
+        },
+        respostaCorreta: "b",
+    },
+    {
+        pergunta: "9. Em qual livro um piloto de avião conhece um jovem príncipe de outro planeta?",
+        respostas: {
+            a: "O Pequeno Príncipe",
+            b: "Harry Potter e o Cálice de Fogo",
+            c: "O Hobbit",
+            d: "Cem Anos de Solidão",
+        },
+        respostaCorreta: "a",
+    },
+    {
+        pergunta: "10. Qual livro é um romance que explora as ilusões e desilusões amorosas de uma mulher?",
+        respostas: {
+            a: "O Primo Basílio",
+            b: "Memórias Póstumas de Brás Cubas",
+            c: "O Guarani",
+            d: "Dom Casmurro",
+        },
+        respostaCorreta: "a",
+    },
+];
---
+++ script/script.js
@@ -0,0 +1,3 @@
+import { perguntasFilmes } from "./module/filmes.js";
+import { perguntasLivros } from "./module/livros.js";
+import { perguntasSeries } from "./module/series.js";
