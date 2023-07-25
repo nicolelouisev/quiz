6dd71cbb433674e5f22dc0de3a5dd762b87f247d
thaisboscolo <thaisboscolo@hotmail.com>
2023-07-25 19:33:40 -0300

    audio

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+        function playAudio() {
+            let x = document.getElementById("myAudio");
+            x.play();
+        }
+        function pauseAudio() {
+            let x = document.getElementById("myAudio");
+            x.pause();
+        }  
